abf3ae3291e6658f35317b060adea0d5be03d164
rdias66 <dias.a@live.com>
2023-05-11 15:05:52 -0300

    first commit, hello world

---
+++ 01-classic/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := "Hello World"
+	var s2 = "Greetins, planet"
+	var s3 string = "Hi earth"
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+}
