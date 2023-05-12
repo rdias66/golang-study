cdcd20607278a1bbe8c34da980f7b5268053dbd8
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-05-12 08:22:06 -0300

    Update main.go

--- 02-weather-api/main.go
+++ 02-weather-api/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"./src/weather.service"
+	"./src/user.service"
 )
 
 func main () {
-	city_name := "User input for city"
-	country_code := "User input for country"
+	city_name , country_code := inputInfo()
+
 	weather_response , temperature_response := getWeather(city_name, country_code) 
 }
