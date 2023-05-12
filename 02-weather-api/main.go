8d481e169633e7b52f56c191de2e4dd37a03d866
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-05-12 08:23:05 -0300

    Update main.go

--- 02-weather-api/main.go
+++ 02-weather-api/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"./src/weather.service"
 	"./src/user.service"
+	"./src/printer.service"
 )
 
 func main () {
 	city_name , country_code := inputInfo()
 
 	weather_response , temperature_response := getWeather(city_name, country_code) 
+	
+	printer(weather_response, temperature_response)
 }
