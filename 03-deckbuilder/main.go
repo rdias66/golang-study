be8a30512da5b8149427986a8e0a9a676d5728ea
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-07 12:26:21 -0300

    Create main.go

---
+++ 03-deckbuilder/main.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+    "fmt"
+)
+
+func main() {
+  aces := SuitBuilder("of Aces")
+  clubs := SuitBuilder("of Clubs")
+  hearts := SuitBuilder("of Hearts")
+  spades := SuitBuilder("of Spades")
+  
+}
