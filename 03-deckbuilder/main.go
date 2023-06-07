1007bb430ed1f85c148cef6984db0fd16aa31ed7
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-07 12:43:42 -0300

    Update main.go

--- 03-deckbuilder/main.go
+++ 03-deckbuilder/main.go
@@ -5,9 +5,16 @@ import (
 )
 
 func main() {
-  aces := SuitBuilder("of Aces")
   clubs := SuitBuilder("of Clubs")
+  diamonds := SuitBuilder("of Diamonds")
   hearts := SuitBuilder("of Hearts")
   spades := SuitBuilder("of Spades")
-  
+  suits := []suit{clubs, diamonds, hearts, spades}
+  deck := DeckBuilder(suits)
+    
+    //to be deck printer
+    
+    for _, d := range deck {
+        fmt.Println(d)
+    }
 }
