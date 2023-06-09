69d052d02f6cf8b302d72acaba23969c5dfb99d3
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-09 08:26:22 -0300

    card type struct created and applied

--- 03-deckbuilder/suit.controller.go
+++ 03-deckbuilder/suit.controller.go
@@ -2,30 +2,36 @@ package main
 
 import "fmt"
 
-type card string
+type card struct {
+	value string
+	suit  string
+}
 
 type suit []card
 
 func SuitBuilder(suitName string) suit {
-	suitName = " " + suitName
 	newSuit := suit{}
 	for i := 1; i <= 13; i++ {
 		if i == 1 {
-			var newCard card = card("Ace of" + suitName)
+			var newCard card = NewCard("Ace of ", suitName)
 			newSuit = append(newSuit, newCard)
 		} else if i == 11 {
-			var newCard card = card("Jack of" + suitName)
+			var newCard card = NewCard("Jack of ", suitName)
 			newSuit = append(newSuit, newCard)
 		} else if i == 12 {
-			var newCard card = card("Queen of" + suitName)
+			var newCard card = NewCard("Queen of ", suitName)
 			newSuit = append(newSuit, newCard)
 		} else if i == 13 {
-			var newCard card = card("King of" + suitName)
+			var newCard card = NewCard("King of ", suitName)
 			newSuit = append(newSuit, newCard)
 		} else {
-			var newCard card = card(fmt.Sprint(i) + suitName)
+			var newCard card = NewCard(fmt.Sprint(i), suitName)
 			newSuit = append(newSuit, newCard)
 		}
 	}
 	return newSuit
+}
+
+func NewCard(value string, suit string) card {
+	return card{value, suit}
 }
