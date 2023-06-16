21754ef5f77167c1c66869fff81ff430d39e483c
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-16 10:40:54 -0300

    Update main.go

--- 03-deckbuilder/main.go
+++ 03-deckbuilder/main.go
@@ -7,17 +7,42 @@ func main() {
 	diamonds := SuitBuilder("Diamonds")
 	hearts := SuitBuilder("Hearts")
 	spades := SuitBuilder("Spades")
-	suits := []suit{clubs, diamonds, hearts, spades}
-	deck := DeckBuilder(suits)
+	var suitChecker int = 0
+	if checkSuitSize(clubs)
+		suitChecker++
+	if checkSuitSize(diamonds)
+		suitChecker++
+	if checkSuitSize(hearts)
+		suitChecker++
+	if checkSuitSize(spades)
+		suitChecker++
 
-	DeckPrinter(deck)
-
-	deckSize := len(deck)
-	fmt.Println(deckSize)
+	var deck Deck
+	
+	if (suitChecker == 4) {
+		suits := []suit{clubs, diamonds, hearts, spades}
+		deck = DeckBuilder(suits)
+	}
+	else {
+		fmt.Println("Suits size are not valid")
+	}
+	
+	if checkDeckSize(deck)
+		DeckPrinter(deck)
+	
+	if !checkDeckSize(deck)
+		fmt.Println("Deck size invalid")
 }
 
 func checkSuitSize(s suit) bool{
-	if(len(suit) == 13){
+	if(len(s) == 13){
+		return true	
+	}
+	return false
+}
+
+func checkDeckSize(d deck) bool{
+	if(len(d) == 52){
 		return true	
 	}
 	return false
